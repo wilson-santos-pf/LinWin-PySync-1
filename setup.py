5f41d2d303ee63262104fd827aeb93cb67292690
imtal <imtal@yolt.nl>
2015-01-05 18:53:49 +0100

    Icons in package

--- setup.py
+++ setup.py
@@ -3,17 +3,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
-config = {
-    'description': 'LocalBox sync client',
-    'author': 'Tjeerd van der Laan',
-    'url': 'http://github.com/2EK/Linux-Sync',
-    'download_url': 'https://github.com/2EK/Linux-Sync/archive/master.zip',
-    'author_email': 'imtal@yolt.nl',
-    'version': '0.1',
-    'install_requires': ['nose'],
-    'packages': ['lox','lox.gui','daemon','iso8601'],
-    'scripts': ['lox-client'],
-    'name': 'lox-client'
-}
-
-setup(**config)
+setup(
+    description = 'LocalBox sync client',
+    author = 'Tjeerd van der Laan',
+    url = 'http://github.com/2EK/Linux-Sync',
+    download_url = 'https://github.com/2EK/Linux-Sync/archive/master.zip',
+    author_email = 'imtal@yolt.nl',
+    version = '0.1',
+    install_requires = ['nose'],
+    packages = ['lox','lox.gui','daemon','iso8601'],
+    include_package_data = True,
+    package_data = {
+        'lox.gui': ['*.png']
+    },
+    scripts = ['lox-client'],
+    name = 'lox-client'
+)

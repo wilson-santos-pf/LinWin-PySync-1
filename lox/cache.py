ce203bc6137308fbe8777f5cb457a198d30ceabb
imtal <imtal@yolt.nl>
2015-01-21 20:08:07 +0100

    Cache consistency check

--- lox/cache.py
+++ lox/cache.py
@@ -1,26 +1,54 @@
 '''
-Module levert een eenvoudige cahce gebaseerd op shelve
+Module levert een eenvoudige cache gebaseerd op shelve
+Bij openen wordt gecontroleerd op versienummer en
+of het dezelfde directory betreft, anders wordt de cache
+geleegd (want niet geldig).
 
 Usage:
-    s = LoxCache("filename")
-    s["key"] = "valus" # automatically does sync()
+    s = LoxCache(name) # determines location from config
+    s["key"] = "values" # automatically does sync()
     del s["key"] # okay even if key does not exist
 
 '''
 
+import os
+import lox.config as config
+from lox.api import LoxApi
 from shelve import DbfilenameShelf
 
 class LoxCache(DbfilenameShelf):
-    
+
     # default to newer pickle protocol and writeback=True
-    def __init__(self, filename, protocol=2, writeback=True):
-        DbfilenameShelf.__init__(self, filename, protocol=protocol, writeback=writeback)
-    
+    def __init__(self, name, logger):
+        filename = os.environ['HOME'] + '/.lox/' + name + '.cache'
+        DbfilenameShelf.__init__(self, filename, protocol=2, writeback=True)
+        api = LoxApi(name)
+        api_version = api.version()
+        config_dir = config.settings[name]['local_dir']
+        try:
+            my_dir = self.get('local_dir',None)
+            assert config_dir == my_dir
+            my_version = self.get('version',None)
+            assert api_version == my_version
+        except AssertionError:
+            logger.warn("Initializing cache")
+            self.clear()
+            self['local_dir'] = config_dir
+            self['version'] = api_version
+
+    def __del__(self):
+        self.close()
+
     def __setitem__(self, name, value):
         key = name.encode('utf8')
         DbfilenameShelf.__setitem__(self, key, value)
         self.sync()
-    
+
+    def __getitem__(self, name):
+        key = name.encode('utf8')
+        value = DbfilenameShelf.__getitem__(self, key)
+        return value
+
     def __delitem__(self, name):
         key = name.encode('utf8')
         if DbfilenameShelf.has_key(self,key):

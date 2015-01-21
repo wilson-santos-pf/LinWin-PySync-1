a2422220201968ce62fa9d68a57177972d9a3368
imtal <imtal@yolt.nl>
2015-01-21 20:07:01 +0100

    Correct icon ref

--- lox/gui/gnome.py
+++ lox/gui/gnome.py
@@ -27,7 +27,7 @@ LOGLEVELS = ["none","error","warn","info","debug","traffic"]
 INFO = gtk.MESSAGE_INFO
 ERROR = gtk.MESSAGE_ERROR
 
-def icon(ref='briefcase',size=32):
+def icon(ref='localbox',size=32):
     this = os.path.realpath(__file__)
     path = os.path.dirname(this)
     filename = '{0}_{1}.png'.format(ref,size)

dda889a04fadc285ecec40aed77fd12b8b96edc7
imtal <imtal@yolt.nl>
2015-02-02 22:25:10 +0100

    Crypto helper corrected

--- lox/crypto.py
+++ lox/crypto.py
@@ -5,15 +5,17 @@ Please note:
 (1) The files are AES encrypted
 (2) For that reason, the file is aligned (padded) to a block size of 16
 (3) The original length is never stored, so decryption leaves a padded file
-(4) The key and initialization vector are PGP encrypted
-(5) PGP public and private (!) key are stored on the server
-(6) PGP keys are not signed
-(7) PGP keys and AES keys are stored base64 on the server, module base64 is used
-(8) The ~/.lox directory is used for the keyring
+(4) The initialization vector is stored with the key and not in the file
+(5) The key and initialization vector are PGP encrypted
+(6) PGP public and private (!) key are stored on the server
+(7) PGP keys are not signed
+(8) PGP keys and AES keys are stored base64 on the server, module base64 is used
+(9) The ~/.lox directory is used for the keyring
 
 '''
 
 import base64
+import md5
 from Crypto.Cipher import AES
 from Crypto import Random
 import gnupg
@@ -25,7 +27,7 @@ def set_passphrase(p):
     Store a passphrase for use
     '''
     global __passphrase
-    __passphrase = p
+    __passphrase = md5.new(p).digest()
 
 def get_passphrase(p):
     '''
@@ -35,30 +37,34 @@ def get_passphrase(p):
     return __passphrase
 
 
+__gpg = gnupg.GPG(gnupghome=os.environ['HOME']+'/.lox',
+                    keyring='lox-client',
+                    verbose=False,
+                    options=['--allow-non-selfsigned-uid'])
+
 def gpg_import(key):
     '''
     Import a localbox key in the keyring
     '''
-    gpg = gnupg.GPG(gnupghome='keyrings', verbose=False, options=['--allow-non-selfsigned-uid'])
-    gpg.import_keys(key)
+    __gpg.import_keys(key)
 
 def gpg_decrypt(string, passphrase):
     '''
     PGP decrypt a string (usually the AES key)
     '''
     ciphertext = base64.b64decode(string)
-    plaintext = gpg.decrypt(ciphertext, passphrase)
+    plaintext = __gpg.decrypt(ciphertext, passphrase)
     return plaintext
 
 def gpg_encrypt(string, passphrase):
     '''
     PGP encrypt a string (usually the AES key)
     '''
-    ciphertext = gpg.encrypt(string, passphrase)
+    ciphertext = __gpg.encrypt(string, passphrase)
     encoded = base64.b64encode(ciphertext)
     return encoded
 
-def get_aes_iv():
+def aes_new_iv():
     '''
     Get a new iv as localbox uses a self generated iv
     '''
@@ -81,28 +87,28 @@ def aes_encrypt(key, iv, filename_in, filename_out, chunksize=64*1024):
     '''
     Encrypt a file with AES to another file,
     use function to decrypt from original file to temp file
+    Note: initialization vector and original size are not stored
     '''
     encryptor = AES.new(key, AES.MODE_CBC, iv)
     with open(filename_in, 'rb') as infile:
         with open(filename_out, 'wb') as outfile:
-            #outfile.write(struct.pack('<Q', filesize))
-            #outfile.write(iv)
             while True:
                 chunk = infile.read(chunksize)
                 if len(chunk) == 0:
                     break
                 elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - len(chunk) % 16)
+                outfile.write(encryptor.encrypt(chunk))
 
 def aes_decrypt(key, iv, filename_in, filename_out, chunksize=64*1024):
     '''
     Decrypt a file with AES to another file,
     use function to decrypt from temp file to final file
+    Note: initialization vector and original size are not stored
     '''
     encryptor = AES.new(key, AES.MODE_CBC, iv)
     with open(filename_in, 'rb') as infile:
         with open(filename_out, 'wb') as outfile:
-            #outfile.write(struct.pack('<Q', filesize))
-            #outfile.write(iv)
             while True:
                 chunk = infile.read(chunksize)
                 if len(chunk) == 0:

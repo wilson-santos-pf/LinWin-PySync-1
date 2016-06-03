dc1f9d3ff7ea0b0dd1136c9fbd70dea40ec4722c
Ivo Tamboer <ivo@libbit.eu>
2016-06-03 11:37:12 +0200

    autopep8

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
 from setuptools import setup, find_packages
 from platform import system
 from distutils.sysconfig import get_python_lib
+from subprocess import check_output
 
 data_files = [('localbox', ['localbox.ico'])]
 
 if system() == 'Windows' or system().startswith('CYGWIN'):
     data_files += [('gpg', ['libs/iconv.dll', 'libs/gpg.exe'])]
 
+try:
+    git_number = len([elem for elem in check_output(
+        ['git', 'log']).split("\n") if elem.startswith('commit ')])
+    if git_number != 0:
+        versionno = "0.1a" + str(git_number)
+except WindowsError:
+    print "Sorry; git executable needed for a version number"
+        versionno = "0.1a.nogit"
+
+
 setup(
-    name = "LocalBoxSync",
-    version = "0.1a006",
-    packages = find_packages(),
-    py_modules = ['gnupg'],
-    author = "Letshare Holding B.V.",
-    author_email = "ivo@libbit.eu",
-    description = "",
-    license =  "all rights reserved",
-    url = "http://box.yourlocalbox.org",
-    data_files = data_files
+    name="LocalBoxSync",
+    version=versionno,
+    packages=find_packages(),
+    py_modules=['gnupg'],
+    author="Letshare Holding B.V.",
+    author_email="ivo@libbit.eu",
+    description="",
+    license="all rights reserved",
+    url="http://box.yourlocalbox.org",
+    data_files=data_files
 )

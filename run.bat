561eaab4591a0705a051d5f392cbf0a9691e9a6e
wilson-santos-pf <wilson.santos@penguinformula.com>
2016-11-03 17:19:40 +0000

    rebuilt docs

--- run.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0
-python.exe -m desktop_utils %1
-rem set /p temp="Hit enter
--- sync/models/label_model.py
+++
@@ -1,6 +0,0 @@
-from sync.database import database_execute
-
-
-def delete_client_data(label):
-    sql = 'delete from sites where site=?'
-    database_execute(sql, (label,))
